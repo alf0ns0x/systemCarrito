132ce4a0bdcf82f8ff60b4580b116013e8a55935
Jhumklin Alfonso Rivera  Quispe <jhon_leo_41@hotmail.com>
2024-06-10 19:07:09 +0200

    scriptSql, Creas CapaDatos/CD_Categoria.cs, Creas CapaNegocio/CN_Categoria.cs, MantenedorController.cs, Categoria.cshtml

--- SystemCarritoCompras/capa_PresentacionAdmin/Controllers/MantenedorController.cs
+++ SystemCarritoCompras/capa_PresentacionAdmin/Controllers/MantenedorController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CapaEntidad;
+using CapaNegocio;
+
 namespace capa_PresentacionAdmin.Controllers
 {
     public class MantenedorController : Controller
@@ -24,6 +27,45 @@ namespace capa_PresentacionAdmin.Controllers
             return View();
         }
 
-       
+
+        [HttpGet]
+        public JsonResult ListarCategorias()    
+        {
+            List<Categoria> olista = new List<Categoria>();
+            olista = new CN_Categoria().Listar();
+
+            return Json(new { data = olista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarCategoria(Categoria objeto)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (objeto.IdCategoria == 0)
+            {
+                resultado = new CN_Categoria().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Categoria().Editar(objeto, out mensaje);
+            }
+
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult EliminarUsuario(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Categoria().Eliminar(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+
+        }
     }
 }
