8e3318cd5a66e22837200b6f1d9a4cbcd4576a3e
Jhumklin Alfonso Rivera  Quispe <jhon_leo_41@hotmail.com>
2024-06-12 17:10:19 +0200

    19-CapaNegocio/CN_Producto.cs, MantenedorController.cs, CapaEntidad/Producto.cs, Web.config

--- SystemCarritoCompras/capa_PresentacionAdmin/Controllers/MantenedorController.cs
+++ SystemCarritoCompras/capa_PresentacionAdmin/Controllers/MantenedorController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 using CapaEntidad;
 using CapaNegocio;
+using Newtonsoft.Json;
 
 namespace capa_PresentacionAdmin.Controllers
 {
@@ -108,6 +112,104 @@ namespace capa_PresentacionAdmin.Controllers
             string mensaje = string.Empty;
 
             respuesta = new CN_Marca().Eliminar(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+
+        }
+        #endregion
+
+
+        //PRODUCTO
+        #region PRODUCTO
+        [HttpGet]
+        public JsonResult ListarProducto()
+        {
+            List<Producto> olista = new List<Producto>();
+            olista = new CN_Producto().Listar();
+
+            return Json(new { data = olista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarProducto(string objeto, HttpPostedFileBase archivoImagen)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+            bool operacion_exitosa = true;
+            bool guardar_imagen_exito = true;
+
+            Producto oProducto = new Producto();
+            oProducto = JsonConvert.DeserializeObject<Producto>(objeto);
+
+            decimal precio;
+
+            if (decimal.TryParse(oProducto.PrecioTexto, NumberStyles.AllowDecimalPoint, new CultureInfo("es-PE"), out precio))
+            {
+                oProducto.Precio = precio;
+            }
+            else 
+            {
+                return Json(new { operacionExitosa = false, mensaje = "El formato del precio debe ser ##.##"}, JsonRequestBehavior.AllowGet);
+            }
+
+            if (oProducto.IdProducto == 0)
+            {
+                int idProductoGenerado = new CN_Producto().Registrar(oProducto, out mensaje);
+
+                if (idProductoGenerado != 0)
+                {
+                    oProducto.IdProducto = idProductoGenerado;
+                }
+                else {
+                    operacion_exitosa = false;
+                }
+            }
+            else
+            {
+                operacion_exitosa = new CN_Producto().Editar(oProducto, out mensaje);
+            }
+
+            if (operacion_exitosa) {
+                if (archivoImagen != null) {
+                    string ruta_guardar = ConfigurationManager.AppSettings["ServidorFotos"];
+                    string extension = Path.GetExtension(archivoImagen.FileName);
+                    string nombre_imagen = string.Concat(oProducto.IdProducto.ToString(), extension);
+
+                    try
+                    {
+                        archivoImagen.SaveAs(Path.Combine(ruta_guardar, nombre_imagen));
+                    }
+                    catch (Exception ex)
+                    {
+                        string msg = ex.Message;
+                        guardar_imagen_exito = false;
+                    }
+
+                    if (guardar_imagen_exito)
+                    {
+                        oProducto.RutaImagen = ruta_guardar;
+                        oProducto.NombreImagen = nombre_imagen;
+                        bool rpta = new CN_Producto().GuardarDatosImagen(oProducto, out mensaje);
+
+                    }
+                    else
+                    {
+                        mensaje = "El guardo el producto. Sin embargo, se detectaron problemas con la imagen.";
+                    }
+                }
+            }
+
+            return Json(new { operacionExitosa = operacion_exitosa, idGenerado = oProducto.IdProducto, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult EliminarProducto(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Producto().Eliminar(id, out mensaje);
 
             return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
 
