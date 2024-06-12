e399e855ddcd5d26a167e098e5a09e77ff954e12
Jhumklin Alfonso Rivera  Quispe <jhon_leo_41@hotmail.com>
2024-06-12 18:27:36 +0200

    20-CN_Recursos.cs, MantenedorController.cs, Producto.cshtml

--- SystemCarritoCompras/CapaNegocio/CN_Recursos.cs
+++ SystemCarritoCompras/CapaNegocio/CN_Recursos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -67,6 +68,23 @@ namespace CapaNegocio
             }
 
             return resultado;
+        }
+
+        public static string ConvertirBase64(string ruta, out bool conversion)
+        {
+            string textoBase64 = string.Empty;
+            conversion = true;
+
+            try
+            {
+                byte[] bytes = File.ReadAllBytes(ruta);
+                textoBase64 = Convert.ToBase64String(bytes);
+            }
+            catch 
+            {
+                conversion = false;   
+            }
+            return textoBase64;
         }
     }
 }
--- SystemCarritoCompras/capa_PresentacionAdmin/Controllers/MantenedorController.cs
+++ SystemCarritoCompras/capa_PresentacionAdmin/Controllers/MantenedorController.cs
@@ -202,6 +202,24 @@ namespace capa_PresentacionAdmin.Controllers
             return Json(new { operacionExitosa = operacion_exitosa, idGenerado = oProducto.IdProducto, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
         }
 
+        [HttpPost]
+        public JsonResult ImagenProducto(int id)
+        {
+            bool conversion;
+            Producto oproducto = new CN_Producto().Listar().Where(p => p.IdProducto == id).FirstOrDefault();
+
+            string textoBase64 = CN_Recursos.ConvertirBase64(Path.Combine(oproducto.RutaImagen,oproducto.NombreImagen), out conversion);
+
+            return Json(new
+            {
+                conversion = conversion,
+                textoBase64 = textoBase64,
+                extension = Path.GetExtension(oproducto.NombreImagen)
+            },
+                JsonRequestBehavior.AllowGet
+            );
+        }
+
 
         [HttpPost]
         public JsonResult EliminarProducto(int id)
