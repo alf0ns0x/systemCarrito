98c1e0bb388ed886b51a00282bb4691726810773
Alfonso <jhon_leo_41@hotmail.com>
2024-07-03 23:14:13 +0200

    28-form-cambiarClave

--- SystemCarritoCompras/capa_PresentacionAdmin/Controllers/AccesoController.cs
+++ SystemCarritoCompras/capa_PresentacionAdmin/Controllers/AccesoController.cs
@@ -43,11 +43,62 @@ namespace capa_PresentacionAdmin.Controllers
             }
             else
             {
+                if (oUsuario.Reestablecer)
+                {
+                    TempData["IdUsuario"] = oUsuario.IdUsuario;
+                    return RedirectToAction("CambiarClave");
+                }
+
                 ViewBag.Error = null;
 
                 return RedirectToAction("Index", "Home");
             }
 
+        }
+
+
+        [HttpPost]
+        public ActionResult CambiarClave(string idusuario, string claveactual, string nuevaclave, string confirmarclave)    
+        {
+            Usuario oUsuario = new Usuario();
+
+            oUsuario = new CN_Usuarios().Listar().Where(u => u.IdUsuario == int.Parse(idusuario)).FirstOrDefault();
+
+            if (oUsuario.Clave != CN_Recursos.ConvertirSha256(claveactual))
+            {
+                TempData["IdUsuario"] = idusuario;
+                ViewData["vclave"] = string.Empty;
+                ViewBag.Error = "La contrasena actual no es correcta.";
+                return View();
+            }
+            else if (nuevaclave != confirmarclave)
+            {
+                TempData["IdUsuario"] = idusuario;
+                ViewData["vclave"] = claveactual;
+                ViewBag.Error = "Las contrasenas no coinciden";
+                return View();
+            }
+            ViewData["vclave"] = string.Empty;
+
+
+            nuevaclave = CN_Recursos.ConvertirSha256(nuevaclave);
+
+            string mensaje = string.Empty;
+
+            bool respuesta = new CN_Usuarios().CambiarClave(int.Parse(idusuario), nuevaclave, out mensaje);
+
+            if (respuesta)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["IdUsuario"] = idusuario;
+                ViewBag.Error = mensaje;
+                return View();
+            }
+
+    
         }
 
     }
